22a94055749b39d13c1bb67210e9030d4c387838
Bruno Cabral <cabral.brc@gmail.com>
2021-11-13 11:16:34 -0800

    convert edge to adjancy, hasPath without infinity loop

---
+++ 03_undirectedPath.js
@@ -0,0 +1,42 @@
+const undirectedPath = (edges, nodeA, nodeB) => {
+    // converting edges to adjancy
+    const graph = buildGraph(edges);
+    return hasPath_depth_preventCyclic(graph, nodeA, nodeB, new Set());
+}
+
+// converts edges to adjancy
+const buildGraph = (edges) => {
+    const graph = {};
+
+    for (const edge of edges) {
+        const [a, b] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+}
+
+const hasPath_depth_preventCyclic = (graph, src, dst, visited) => {
+    if (src === dst) return true;
+    if (visited.has(src)) return false;
+    visited.add(src);
+    for(let neighbor of graph[src]){
+        if(hasPath_depth_preventCyclic(graph, neighbor, dst, visited)===true){
+            return true
+        }
+    }
+    return false;
+}
+
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n'],
+]
+
+console.log(undirectedPath(edges, 'j', 'm'));
+console.log(undirectedPath(edges, 'j', 'o'));
